7f33c12be64c59a4cff2cb758a63c95f44f456f5
S.Sudhakar <sudhakar.santhanam@gmail.com>
2015-05-22 12:48:10 +0530

    Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  I prefer to catch the matrix, set, get it and  inverse store both.
+## We can catch the matrix, inverse and store it
 
-## Write a short comment describing this function
+##  working directory is set and the file is read. 
+
+## makecath matrix will catch a matrix and inverse the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  
+  get<-function() x
+  #store the matrix
+  setmatrix<-function ()x
+  getmatrix<-function() x
+  ## inverse of the matrix
+  catchinverse()<-function(solve) m<-solve
+  #get the inverse matrix
+  getinverse<-function()m
+  #return a list
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix
+       catchinverse=catchinverse
+       getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## #cacheSolve: This function computes the inverse of the 
+#special "matrix" returned by makeCacheMatrix 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(x=matrix(), ...) {
+  #get the catched value
+  m<-x$getinverse()
+  # Catch value exists, return 
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  #otherwise, catch the matix
+  data<-x$getmatrix()
+  #get the inverse of the matrix
+  dat1<-solve(data, ...)
+  Y$catchinverse(dat1)
+  #return the value
+  dat1
+
+  
+  ## Return a matrix that is the inverse of 'x'
 }
